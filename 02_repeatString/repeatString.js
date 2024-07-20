6848f34ae431fedbda02c19806c396e537c1a65b
Keith Edwards <89081584+kedwards1306@users.noreply.github.com>
2024-07-20 17:52:05 -0400

    repeatString is done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+if (word < 0) {
+    return "ERROR";
+}
+let string = "";
+for (let i = 0; i < times; i++) {
+    string += word;
+}
+return string;
 
 };
 
