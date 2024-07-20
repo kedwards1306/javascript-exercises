7bef4c320dd0e00c85459a3420149e4580aaeee4
Keith Edwards <89081584+kedwards1306@users.noreply.github.com>
2024-07-20 18:52:54 -0400

    Remove a vlaue from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    //creating a new array
+    const newArray=[];
+    array.forEach((item) => {
+        //if the current item is not in the arguments, add it to the new array
+        if(!args.includes(item)){
+            newArray.push(item);
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
