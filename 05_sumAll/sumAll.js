23de652183bd13853082f0fff98b38cfff6855b9
Keith Edwards <89081584+kedwards1306@users.noreply.github.com>
2024-07-20 23:29:05 -0400

    Sum numbers with the range of an array

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0;
+    
+    // Swap if min is greater than max
+    if (min > max) {
+        [min, max] = [max, min];
+    }
 
-};
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
